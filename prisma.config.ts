21ba3bdc8b8043fc0fc0867d0fafc7a36e47326e
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-04-08 06:49:44 -0500

    Fix: Actualiza configuración de conexión a la BD para Prisma 7

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
     url: process.env.DATABASE_URL,
+    // @ts-ignore
+    directUrl: process.env.DIRECT_URL,
   },
 });
